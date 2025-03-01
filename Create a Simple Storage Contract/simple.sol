096af5565e5bc11f7b2b1b1700402f284c5e5ede
colemanwhaylon <colemanwhaylon@yahoo.com>
2025-02-28 23:09:24 -0800

    Completed code for simple storage contract.

---
+++ Create a Simple Storage Contract/simple.sol
@@ -0,0 +1,22 @@
+// SPDX-License-identifier: MIT
+pragma solidity ^0.8.25;
+
+contract SimpleStorage {
+    // State variable to store a number
+    uint256 private storedData;
+
+    // Event to log when the stored value is changed
+    event ValueChanged(uint256 newValue);
+
+    // Function to store a number
+    function store(uint256 _value) public {
+        storedData = _value;
+        emit ValueChanged(_value);
+    }
+
+    // Function to retrieve the stored number
+    function retrieve() public view returns(uint256)
+    {
+        return storedData;
+    }
+}
